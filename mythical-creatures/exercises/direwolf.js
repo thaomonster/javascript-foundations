eb1e1cb0b242c893412428ae0ace16d777cf33fb
Thao Ma <MaThao@gmail.com>
2020-10-12 17:40:24 -0600

    Finish Direwolf Test

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -1,7 +1,25 @@
+const Stark = require("./stark");
+
 class Direwolf {
-  constructor(name) {
+  constructor(name, home, size) {
     this.name = name;
-    this.home = 'Beyond the Wall'
+    this.home = home || 'Beyond the Wall';
+    this.size = size || 'Massive';
+    this.starksToProtect = [];
+    this.huntsWhiteWalkers = true;
+  };
+
+  protect(stark) {
+    if (this.home === stark.location && this.starksToProtect.length < 2) {
+    this.starksToProtect.push(stark);
+    stark.safe = true;
+    this.huntsWhiteWalkers = false;
+    };
+  };
+
+  leave(stark) {
+    this.starksToProtect = [];
+    stark.safe = false;
   };
 };
 
